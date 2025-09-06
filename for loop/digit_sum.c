d2deb89d0239bcf9f1593b79dc699f96c0a436d7
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-07 00:09:20 +0530

    Update digit_sum.c

--- for loop/digit_sum.c
+++ for loop/digit_sum.c
@@ -4,10 +4,10 @@ int main() {
 	printf("Enter starting : ");
 	scanf("%d",&n);
 	int sum=0;
-	int rem;
 	for(; n>0; n/=10) {
-		rem=n%10;
+		int rem=n%10;
 		sum+=rem;
+		
 	}
     printf("%d",sum);
 	return 0;
