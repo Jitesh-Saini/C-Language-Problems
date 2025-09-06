18e94a9e8ec848211b4a8f724fa8f8ee26738f80
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-06 23:47:56 +0530

    Update m_to_n.c

--- for loop/m_to_n.c
+++ for loop/m_to_n.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("Enter starting : ");
+	scanf("%d",&n);
+	int m;
+	printf("Enter ending : ");
+	scanf("%d",&m);
+	for(int i=n; i<=m; i++) {
+		printf("%d ",i);
+	}
 
+	return 0;
+}
