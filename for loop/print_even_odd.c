be81f8daf037af33eb89599123d57ad32505d8e5
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-06 23:53:38 +0530

    Update print_even_odd.c

--- for loop/print_even_odd.c
+++ for loop/print_even_odd.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("Enter starting : ");
+	scanf("%d",&n);
+	
+	int m;
+	printf("Enter ending : ");
+	scanf("%d",&m);
+	
+	printf("Even : ");
+	
+	for(int i=n; i<=m; i++) {
+		if(i%2==0) printf("%d ",i);
+	}
 
+    printf("\nOdd");
+    for(int i=n; i<=m; i++) {
+		if(i%2!=0) printf("%d ",i);
+	}
+
+	return 0;
+}
