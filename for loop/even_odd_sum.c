dc1c4dbe0a33afaa76ecc8181fad6192333faa5a
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-07 00:03:13 +0530

    Update even_odd_sum.c

--- for loop/even_odd_sum.c
+++ for loop/even_odd_sum.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int main() {
+	int n;
+	printf("Enter starting : ");
+	scanf("%d",&n);
+	
+	int m;
+	printf("Enter ending : ");
+	scanf("%d",&m);
+	
+	printf("Even sum : ");
+	int e_sum=0;
+	for(int i=n; i<=m; i++) {
+		if(i%2==0) e_sum+=i;
+	}
+    printf("%d",e_sum);
 
+    printf("\nOdd sum :");
+    int o_sum=0;
+    for(int i=n; i<=m; i++) {
+		if(i%2!=0) o_sum+=i;
+	}
+	printf("%d",o_sum);
+
+	return 0;
+}
