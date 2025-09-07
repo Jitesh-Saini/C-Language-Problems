e68a575dc33334f7bed59179fe63b93f50ee4654
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-07 11:27:28 +0530

    Create common_factors.c

---
+++ for loop/common_factors.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+
+	int n;
+	printf("Enter n : ");
+	scanf("%d",&n);
+	int m;
+	printf("Enter m : ");
+	scanf("%d",&m);
+	
+	for(int i=2; i<=n||i<=m;) {
+		if(n%i==0 || m%i==0) {
+			printf("%d ",i);
+			if(n%i==0) n=n/i;
+			if(m%i==0) m=m/i;
+		}
+		else i++;
+	}
+
+
+
+
+	return 0;
+}
