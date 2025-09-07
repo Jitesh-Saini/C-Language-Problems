cddf746db216ca019227b82d71934d48e0ffba5d
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-07 11:41:24 +0530

    Update and rename prime_factors_two_numbers.c to lcm_by_factors.c

--- for loop/lcm_by_factors.c
+++ for loop/lcm_by_factors.c
@@ -7,17 +7,18 @@ int main() {
 	int m;
 	printf("Enter m : ");
 	scanf("%d",&m);
-	
+	int lcm=1;
 	for(int i=2; n>1||m>1;) {
 		if(n%i==0 || m%i==0) {
 			printf("%d ",i);
 			if(n%i==0) n=n/i;
 			if(m%i==0) m=m/i;
+			lcm*=i;
 		}
 		else i++;
 	}
 
-
+    printf("\nlcm is : %d",lcm);
 
 
 	return 0;
