f8eb408ee79b18c2d46f853ab11afd0ada23bac2
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-09 19:40:00 +0530

    Create 11.c

---
+++ Patterns/Triangle-Patterns/11.c
@@ -0,0 +1,22 @@
+// 4 
+// 3 3 
+// 2 2 2 
+// 1 1 1 1 
+
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++){
+        int temp=n-i+1;
+        for(int j=1;j<=i;j++){
+            printf("%d ",temp);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
