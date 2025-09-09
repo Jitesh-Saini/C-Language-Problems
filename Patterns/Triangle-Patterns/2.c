615cfbc7dd06557fd211043067dc32b57a0c3856
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-09 18:30:38 +0530

    Create 2.c

---
+++ Patterns/Triangle-Patterns/2.c
@@ -0,0 +1,23 @@
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    for(int i=n;i>=1;i--){
+        int temp=1;
+        for(int j=1;j<=i;j++){
+            printf("%d ",temp);
+            temp++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
