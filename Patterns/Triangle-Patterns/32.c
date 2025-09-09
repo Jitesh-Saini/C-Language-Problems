46e2285f7a2362e1c633666f9c426f05382e3c6c
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-10 01:43:55 +0530

    Update 32.c

--- Patterns/Triangle-Patterns/32.c
+++ Patterns/Triangle-Patterns/32.c
@@ -1,1 +1,28 @@
-
+//  2  4  6  8 
+//   10 12 14 
+//       16 18 
+//          20
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    int temp=2;
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<i;k++){
+            printf("   ");
+        }
+        
+        for(int j=n;j>=i;j--){
+            printf("%2d ",temp);
+            temp+=2;
+        }
+        printf("\n");
+        
+        
+    }
+    
+    return 0;
+}
