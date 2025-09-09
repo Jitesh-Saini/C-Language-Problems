672b431dc01ae13066f8337216379fc1e322f457
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-09 19:42:54 +0530

    Create 10.c

---
+++ Patterns/Triangle-Patterns/10.c
@@ -0,0 +1,22 @@
+// 4 4 4 4 
+// 3 3 3 
+// 2 2 
+// 1 
+
+#include<stdio.h>
+int main() {
+
+	int n;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+
+	for(int i=n; i>=1; i--) {
+		int temp=i;
+		for(int j=1; j<=i; j++) {
+			printf("%d ",temp);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
