d7e215f6ef021c4160477b2dff955203a04d5c93
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-10 01:26:09 +0530

    Update 26.c

--- Patterns/Triangle-Patterns/26.c
+++ Patterns/Triangle-Patterns/26.c
@@ -1,1 +1,24 @@
 
+// 19 17 15 13 
+// 11 9 7 
+// 5 3 
+// 1 
+
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    int temp=n*(n+1)-1;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("%d ",temp);
+            temp-=2;;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
