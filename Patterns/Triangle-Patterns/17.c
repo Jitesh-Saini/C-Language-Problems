39905169eed322e8905144c66e38bb283c6aa4c5
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-10 00:46:38 +0530

    Update 17.c

--- Patterns/Triangle-Patterns/17.c
+++ Patterns/Triangle-Patterns/17.c
@@ -1,1 +1,24 @@
+// 10 
+// 9 8 
+// 7 6 5 
+// 4 3 2 1 
 
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    // int temp=(n%2==0) ? ((n*(n/2))+(n/2)) : ((n*(n/2))+n);
+    int temp=(n*(n+1))/2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",temp);
+            temp--;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
