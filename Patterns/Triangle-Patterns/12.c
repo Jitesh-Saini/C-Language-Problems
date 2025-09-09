648523dd2805a648651c0268393f2d2db25362fa
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-09 19:52:30 +0530

    Create 12.c

---
+++ Patterns/Triangle-Patterns/12.c
@@ -0,0 +1,32 @@
+// 4 3 2 1 
+//   3 2 1 
+//     2 1 
+//       1 
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++){
+        
+        for(int k=1;k<i;k++){
+            printf("  ");
+        }
+        
+        // for(int j=n+1-i;j>=1;j--){
+        //     printf("%d ",n+1-i);
+        // }
+        
+        int temp=n+1-i;
+        for(int j=n;j>=i;j--){
+            printf("%d ",temp);
+        }
+        printf("\n");
+        
+        
+    }
+    
+    return 0;
+}
