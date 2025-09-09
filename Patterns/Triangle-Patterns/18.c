d05de0174712fc1d0c99f08f95b670ebf3f33624
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-10 00:48:55 +0530

    Update 18.c

--- Patterns/Triangle-Patterns/18.c
+++ Patterns/Triangle-Patterns/18.c
@@ -1,1 +1,26 @@
 
+
+// 15 14 13 12 11 
+// 10 9 8 7 
+// 6 5 4 
+// 3 2 
+// 1 
+
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    int temp=(n*(n+1))/2;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("%d ",temp);
+            temp--;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
