6e4d8ceba6c4291b07c6906f66b94783f2827164
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-13 19:50:44 +0530

    Create 8_rhombus_pattern.c

---
+++ Patterns/Advance/8_rhombus_pattern.c
@@ -0,0 +1,26 @@
+// * * * * * 
+//  * * * * * 
+//   * * * * * 
+//    * * * * * 
+//     * * * * * 
+
+#include<stdio.h>
+int main(){
+    
+    int n=5;
+    for(int i=1 ; i<=n; i++){
+        for(int j=2; j<=n; j++){
+            if(j<=i) {
+                printf(" ");
+            }
+            // else printf("  ");
+        }
+        
+        for(int j=1; j<=n; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
