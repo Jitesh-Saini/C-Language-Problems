c2a910bcb27de2c6b1ad54319cdc97e62cf1d50d
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-13 19:45:15 +0530

    Create 7_palindrom_triangale.c

---
+++ Patterns/Advance/7_palindrom_triangale.c
@@ -0,0 +1,27 @@
+//         1         
+//       2 1 2       
+//     3 2 1 2 3     
+//   4 3 2 1 2 3 4   
+// 5 4 3 2 1 2 3 4 5 
+#include<stdio.h>
+int main(){
+    
+    int n=5;
+    for(int i=1 ; i<=n; i++){
+        for(int j=n; j>=1; j--){
+            if(j<=i) {
+                printf("%d ",j);
+            }
+            else printf("  ");
+        }
+        for(int j=2; j<=n; j++){
+            if(j<=i) {
+                printf("%d ",j);
+            }
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
