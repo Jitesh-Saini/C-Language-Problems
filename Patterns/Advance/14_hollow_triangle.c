8e0886c784a47ba7ede620b40abdcbb8175b2916
Abhishek Bairwa <mr.abhi1149@gmail.com>
2025-09-13 20:51:31 +0530

    Create 14_hollow_triangle.c

---
+++ Patterns/Advance/14_hollow_triangle.c
@@ -0,0 +1,29 @@
+//       *       
+//     *   *     
+//   *       *   
+// * * * * * * * 
+
+#include<stdio.h>
+int main(){
+    
+    int n=4;
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=1; j--){
+            if(j<=i) {
+                if(i==j || i==4) printf("* ");
+                else printf("  ");
+            }
+            else printf("  ");
+        }
+        for(int j=2; j<=n; j++){
+            if(j<=i) {
+                if(i==j || i==4) printf("* ");
+                else printf("  ");
+            }
+            else printf("  ");
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
